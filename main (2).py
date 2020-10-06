97e04b53546ede3cc9ce6dd6284c2f7f92243d24
harshithagupta2614 <55712062+harshithagupta2614@users.noreply.github.com>
2020-10-07 02:50:03 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -23,28 +23,16 @@ class Triangle(Polygon):
         self.height=height
         self.area=self.base*self.height*(1/2)
         self.perimeter=length+l2+base
-    
-class Rhombus(Polygon):
-     def __init__(self,length,height):
-        Polygon.__init__(self,length)
-        self.height=height
-        self.area=self.length*self.height
-        self.perimeter=4*self.length
-        
 
     
 r1=Rectangle(15,10)
 s1=Square(6)
-rh1=Rhombus(7,8)
 t1=Triangle(3,5,8,7)
 print("Area of rectangle : ",r1.area)
 print("perimeter of rectangle ",r1.perimeter)
 print(" ")
 print("Area of square : ",s1.area)
 print("perimeter of square ",s1.perimeter)
-print(" ")
-print("Area of rhombus : ",rh1.area)
-print("perimeter of rhombus ",rh1.perimeter)
 print(" ")
 print("Area of triangle : ",t1.area)
 print("perimeter of triangle",t1.perimeter)
